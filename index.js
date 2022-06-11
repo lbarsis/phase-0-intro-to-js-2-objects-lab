50ffaf4c35c55b1e930fdbcb3807d716960b440f
Levi Barsis <lbarsis@gmail.com>
2022-06-11 12:40:59 -0700

    commit tree a74d4c657d5608ed4f22729c5f4aa1313d9d37b7

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+employee = {
+    name: "Sam",
+    streetAddress: "2901 S Sepulveda"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = {...employee}
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+
+
+
